bdbdbd9986c83e7de072848e84f3eefcfa087a8b
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-10-15 17:18:52 +0900

    scope q3

--- scope.js
+++ scope.js
@@ -8,9 +8,19 @@
 
 // 問題２　次のプログラムを実行した時、コンソールには何と表示されるか。
 
+// let a = "hello"
+// function greeting(arg) {
+//   arg = 'Bye'
+//   console.log(arg)
+// }
+// greeting(a);
+
+// 問題3　次のプログラムを実行した時、コンソールには何と表示されるか。
+
 let a = "hello"
-function greeting(arg) {
-  arg = 'Bye'
-  console.log(arg)
+function first(arg1) {
+  a = 'See you'
+  arg1 = 'Bye'
+  console.log(a, arg1)
 }
-greeting(a);
+first(a);
